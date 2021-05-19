8d511799029b26373688da93017cb3a7fc4bfbc6
Rishabh Agrawal <rishabhagrawal41@gmail.com>
2021-05-18 20:33:31 -0400

    update

--- model.py
+++ model.py
@@ -4,12 +4,13 @@ import matplotlib.pyplot as plt
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 print(x_test.shape)
+print(x_test[0])
 
 x_train = tf.keras.utils.normalize(x_train, axis=1)
 x_test = tf.keras.utils.normalize(x_test, axis=1)
 
 model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
 model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
 model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
 model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
@@ -17,7 +18,7 @@ model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 # model.fit(x_train, y_train, epochs=3)
 
-#print(model.evaluate(x_test, y_test))
+# print(model.evaluate(x_test, y_test))
 plt.imshow(x_test[0], cmap=plt.cm.binary)
 plt.show()
 # model.save("detect_number.model")
--- new.py
+++ new.py
@@ -12,16 +12,33 @@ nums = []
 for img in os.listdir(path):
     print(img)
     img_arr = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+    x_inb = 0
+    y_inb = 0
+    for i,row in enumerate(img_arr):
+        for j,val in enumerate(row):
+            print(val)
+            if val > 254:
+                x_inb = j
+                y_inb = i
+                x_out = 36-j
+                y_out = 36-i
+                break
+
+    print(x_inb, x_out, y_inb, y_out)
+    cut_down = [row[y_out:y_inb] for row in img_arr[x_inb:x_out]]
     new_img = cv2.resize(img_arr, (28,28))
     float_img = np.float32(new_img)
     for i,row in enumerate(float_img):
         for j,val in enumerate(row):
             float_img[i][j] = 255-val
-
+    float_img = np.int32(float_img)
+    plt.imshow(cut_down, cmap=plt.cm.binary)
+    plt.show()
     nums.append(float_img)
 
-print(np.shape(nums))
-predictions = model.predict([nums])
+# print(np.shape(nums))
+print(nums[0])
+predictions = model.predict(nums[0])
 for p in predictions:
     print(np.argmax(predictions[0]))
 
